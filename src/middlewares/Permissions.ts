b14c54d4c68e857e542f87d0c84359db1d446113
Alberto <alberto.renteriac@iteso.mx>
2024-10-22 19:08:31 -0600

    ADDED: The permission middleware

--- src/middlewares/Permissions.ts
+++ src/middlewares/Permissions.ts
@@ -1,88 +1,28 @@
 import { Request, Response, NextFunction } from 'express';
 import HTTP_STATUS_CODES from '../types/http-status-codes';
+import ROLES from '../types/roles';
 
-export const adminMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const user = req.user;
+const permissionsMiddleware = (allowedRoles: ROLES[], allowSelf: boolean) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+    const { id } = req.params;
 
-  if (!user) {
-    res
-      .status(HTTP_STATUS_CODES.UNAUTHORIZED)
-      .json({ message: 'Unauthorized' });
-    return;
-  }
+    if (!user) {
+      res
+        .status(HTTP_STATUS_CODES.UNAUTHORIZED)
+        .json({ message: 'Unauthorized' });
+      return;
+    }
 
-  if (user.role === 'admin') {
-    next();
-  } else {
-    res.status(HTTP_STATUS_CODES.FORBIDDEN).json({ message: 'Forbidden' });
-  }
+    if (
+      allowedRoles.includes(user.role!) ||
+      (allowSelf && (user._id as string) === id)
+    ) {
+      next();
+    } else {
+      res.status(HTTP_STATUS_CODES.FORBIDDEN).json({ message: 'Forbidden' });
+    }
+  };
 };
 
-export const moderatorMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const user = req.user;
-
-  if (!user) {
-    res
-      .status(HTTP_STATUS_CODES.UNAUTHORIZED)
-      .json({ message: 'Unauthorized' });
-    return;
-  }
-
-  if (user.role === 'mod') {
-    next();
-  } else {
-    res.status(HTTP_STATUS_CODES.FORBIDDEN).json({ message: 'Forbidden' });
-  }
-};
-
-export const selfOrAdminMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const user = req.user;
-  const { id } = req.params;
-
-  if (!user) {
-    res
-      .status(HTTP_STATUS_CODES.UNAUTHORIZED)
-      .json({ message: 'Unauthorized' });
-    return;
-  }
-
-  if (user.role === 'admin' || (user._id as string) === id) {
-    next();
-  } else {
-    res.status(HTTP_STATUS_CODES.FORBIDDEN).json({ message: 'Forbidden' });
-  }
-};
-
-export const selfOrModeratorMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const user = req.user;
-  const { id } = req.params;
-
-  if (!user) {
-    res
-      .status(HTTP_STATUS_CODES.UNAUTHORIZED)
-      .json({ message: 'Unauthorized' });
-    return;
-  }
-
-  if (user.role === 'mod' || (user._id as string) === id) {
-    next();
-  } else {
-    res.status(HTTP_STATUS_CODES.FORBIDDEN).json({ message: 'Forbidden' });
-  }
-};
+export default permissionsMiddleware;
