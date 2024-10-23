a0aeb08b0dbc85639624cbbeaf910091ced42010
URSUS32 <saul.razo@iteso.mx>
2024-10-23 10:15:07 -0600

    Cors update

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { connectDB } from './config/db';
 import cors from 'cors';
 import routes from './routes';
 import cookieParser from 'cookie-parser';
+
 config();
 connectDB();
 
@@ -11,7 +12,17 @@ const app = express();
 const port = process.env.PORT ?? 3000;
 
 app.disable('x-powered-by');
-app.use(cors());
+
+// Configuración de CORS
+const corsOptions = {
+  origin: 'http://localhost:4200',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+// Uso de CORS
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser(process.env.SECRET_COOKIE_KEY as string));
 app.use(routes);
