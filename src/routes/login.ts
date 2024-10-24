cb7ab03ec51c02a76e1f7ff5f14110bd0899f4d3
Samuel Valentin <samuelv.lopez@iteso.mx>
2024-10-23 19:04:05 -0600

    Descripción clara de los cambios realizados

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -3,6 +3,80 @@ import loginController from '../controllers/loginController';
 
 const loginRoutes = Router();
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: User login
+ *     description: Logs in a user and returns authentication details.
+ *     tags:
+ *       - Login
+ *     requestBody:
+ *       description: User credentials for login
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "user@example.com"
+ *               password:
+ *                 type: string
+ *                 example: "password123"
+ *     responses:
+ *       200:
+ *         description: Login successful and user authenticated with the necessary permissions.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Login successful"
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     email:
+ *                       type: string
+ *                       example: "user@example.com"
+ *                     role:
+ *                       type: string
+ *                       example: "admin"
+ *       403:
+ *         description: User authenticated but does not have the necessary permissions.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Authenticated but insufficient permissions"
+ *       401:
+ *         description: Invalid credentials or user not authenticated.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Invalid username or password"
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Internal Server Error"
+ */
+
 loginRoutes.post('/', loginController.login);
 
 export default loginRoutes;
