7a5c88548bc035b1437315a32469e41c70f60cad
Alberto <alberto.renteriac@iteso.mx>
2024-11-18 18:50:53 -0600

    Now the id_user is retrieved from the token instead of the body

--- src/controllers/commentsController.ts
+++ src/controllers/commentsController.ts
@@ -2,11 +2,34 @@ import commentModel from '../models/Comment';
 import BaseController from '../utils/BaseController';
 import { Comment } from '../models/Comment';
 import { Model } from 'mongoose';
+import { Request, Response } from 'express';
+import HTTP_STATUS_CODES from '../types/http-status-codes';
 
 class CommentsController extends BaseController<Comment> {
   constructor(model: Model<Comment>) {
     super(model);
   }
+
+  update = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const content = req.body;
+
+    content.id_user = req.user!._id;
+
+    this.model
+      .findByIdAndUpdate(id, content, { new: true, runValidators: true })
+      .then((item: Comment | null) => {
+        if (!item) {
+          return res
+            .status(HTTP_STATUS_CODES.NOT_FOUND)
+            .json({ message: 'Item not found' });
+        }
+        return res.status(HTTP_STATUS_CODES.OK).json(item);
+      })
+      .catch((error: any) =>
+        this.handleError(res, error, 'Error updating item')
+      );
+  };
 }
 
 export default new CommentsController(commentModel);
