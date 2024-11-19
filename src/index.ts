cb23eab1e416ba36170677dce3a202054d6b3e31
Alberto <alberto.renteriac@iteso.mx>
2024-11-19 15:34:45 -0600

    Permitted the auth header on cors config

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ const corsOptions = {
   origin: process.env.WEB_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'auth'],
 };
 
 // Uso de CORS
