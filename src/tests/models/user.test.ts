26addbd4d236af2b52e6651a205982a803ced561
URSUS32 <saul.razo@iteso.mx>
2024-11-26 21:38:48 -0600

    Implemented tests

--- src/tests/models/user.test.ts
+++ src/tests/models/user.test.ts
@@ -1,14 +1,21 @@
 import mongoose from 'mongoose';
 import User from '../../models/User'; // Ajusta el path según tu estructura
 
+const createInvalidUser = () => new User(); // Función para crear un usuario inválido
+
+const validateUserError = async (user) => {
+    try {
+        await user.validate();
+    } catch (err) {
+        return err as mongoose.Error.ValidationError;
+    }
+};
+
 describe('User Model Validation', () => {
     it('should throw validation error if required fields are missing', async () => {
-        const user = new User(); // Crear un usuario vacío
-        try {
-            await user.validate();
-        } catch (err) {
-            const error = err as mongoose.Error.ValidationError; // Especifica el tipo aquí
-            expect(error.errors).toHaveProperty('email'); // Verifica que 'email' sea requerido
-        }
+        const user = createInvalidUser();
+        const error = await validateUserError(user);
+        
+        expect(error.errors).toHaveProperty('email'); // Verifica que 'email' sea requerido
     });
 });
--- src/tests/routes/profile.test.ts
+++ src/tests/routes/profile.test.ts
@@ -1,28 +1,34 @@
-import request from 'supertest'; // Paquete para hacer solicitudes HTTP en pruebas
+import request from 'supertest';
 import app from '../../index';
 import HTTP_STATUS_CODES from '../../types/http-status-codes';
 
+const getToken = async () => {
+  const response = await request(app).post('/login').send({
+    email: 'saulrazo900@gmail.com',
+    password: 'ratacalva',
+  });
+  return response.body.token;
+};
+
+const checkProfileResponse = (response) => {
+  expect(response.status).toBe(HTTP_STATUS_CODES.OK);
+  expect(response.body).toHaveProperty('id_city');
+  expect(response.body).toHaveProperty('username');
+  expect(response.body).toHaveProperty('email');
+  expect(response.body).not.toHaveProperty('password');
+};
+
 describe('POST /profile', () => {
   it(`should return ${HTTP_STATUS_CODES.OK} if the token is valid`, async () => {
-    const response = await request(app).post('/login').send({
-      email: 'beto@correo2.com',
-      password: 'salsa159',
-    });
-
-    const token = response.body.token;
-
+    const token = await getToken();
     const profileResponse = await request(app)
       .get('/profile')
       .set('Authorization', `Bearer ${token}`);
-
-    expect(profileResponse.status).toBe(HTTP_STATUS_CODES.OK);
-    expect(profileResponse.body).toHaveProperty('id_city');
-    expect(profileResponse.body).toHaveProperty('username');
-    expect(profileResponse.body).toHaveProperty('email');
-    expect(profileResponse.body).not.toHaveProperty('password');
+    
+    checkProfileResponse(profileResponse);
   });
 
-  it(`should return ${HTTP_STATUS_CODES.FORBIDDEN} if the token is invalid`, async () => {
+  it(`should return ${HTTP_STATUS_CODES.UNAUTHORIZED} if the token is invalid`, async () => {
     const profileResponse = await request(app)
       .get('/profile')
       .set('Authorization', `Bearer invalidtoken`);
